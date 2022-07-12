215a3b867f48022e7aa66dd905eed4e59ce0cbba
Mazy <mazymohamed06@gmail.com>
2022-07-12 15:25:00 +0100

    default layout added for air page

--- pages/Boreal_Exploration_Continued.py
+++ pages/Boreal_Exploration_Continued.py
@@ -1,3 +1,4 @@
+from turtle import color
 import geemap.foliumap as geemap
 import ee
 import streamlit as st
@@ -7,7 +8,7 @@ from PIL import Image
 import time
 import plotly.express as px
 import time
-from streamlit_folium import st_folium
+from earth_observation_v1 import Fire
 import folium
 import os.path
 import pickle as pkle
@@ -41,17 +42,15 @@ st.session_state["errors"] = []
 
 df = pd.read_csv("Data/wildfire_boreal_forest.csv")
 fires_choices = df["name"]
-layers_choices = {"True Color": "true", "False Color": "false",
-                  "SWIR": "swir", "NBR": "nbr", "BAI": "bai",
-                  "EVI": "evi", "Air Quality (Nitrogen Dioxide)": "no2",
+layers_choices = {"Air Quality (Nitrogen Dioxide)": "no2",
                   "Air Quality (Carbon Monoxide)": "co",
                   "LST": "lst"}
 
 st.markdown(
-            """<p style="color:#33ff33; font-size:50px; text-align:center">
+    """<p style="color:#33ff33; font-size:50px; text-align:center">
             The Boreal Forrest Exploration</p>""",
-            unsafe_allow_html=True,
-        )
+    unsafe_allow_html=True,
+)
 
 with st.sidebar:
     fire = st.selectbox("Choose a fire", fires_choices)
@@ -59,61 +58,106 @@ with st.sidebar:
 
     st.write("____________________")
     start_date = str(st.date_input("Start Date",
-                                value=datetime.datetime.strptime(df[df['name'] == fire]["date_start"].iloc[0], "%d/%m/%Y").date(),
-                                min_value=datetime.date(2000, 1, 1),
-                                max_value=datetime.date.today()))
+                                   value=datetime.datetime.strptime(
+                                       df[df['name'] == fire]["date_start"].iloc[0], "%d/%m/%Y").date(),
+                                   min_value=datetime.date(2000, 1, 1),
+                                   max_value=datetime.date.today()))
 
-    end_date=str(st.date_input("End Date",
-                                value=datetime.datetime.strptime(df[df['name'] == fire]["date_end"].iloc[0], "%d/%m/%Y").date(),
-                                min_value = datetime.date(2000, 1, 1),
-                                max_value = datetime.date.today()))
+    end_date = str(st.date_input("End Date",
+                                 value=datetime.datetime.strptime(
+                                     df[df['name'] == fire]["date_end"].iloc[0], "%d/%m/%Y").date(),
+                                 min_value=datetime.date(2000, 1, 1),
+                                 max_value=datetime.date.today()))
 
     st.write("____________________")
 
-    layer=st.selectbox("Choose a layer", layers_choices.keys())
+    layer = st.selectbox("Choose a layer", layers_choices.keys())
 
     if "Air Quality" in layer and pd.to_datetime(start_date) < pd.to_datetime('2017-11-13'):
-            st.markdown(
-                """<p class="small-font" style="color:red">
+        st.markdown(
+            """<p class="small-font" style="color:red">
                     Air Quality layer unavailable before 2017-11-13.</p>""",
-                unsafe_allow_html=True,
-            )
-            st.session_state["errors"].append(True)
+            unsafe_allow_html=True,
+        )
+        st.session_state["errors"].append(True)
 
     st.write("____________________")
 
-    pressed=st.button("Build Map")
+    pressed = st.button("Build Map")
+
+
+if (pressed) and (True not in st.session_state["errors"]):
+
+    fire_object = Fire(df.iloc[df[df['name'] == fire].index[0]])
+    st.session_state["fire_object"] = fire_object
+
+    if "Air Quality" in layer:
+        st.session_state["fire_object_air"].air_quality(
+            start_date=start_date, end_date=end_date, air_indice=layers_choices[layer])
+
+    st.session_state["fire_object_temp"].lst(
+        start_date=start_date, end_date=end_date)
+
+    st.session_state["start_date"] = start_date
+    st.session_state["end_date"] = end_date
 
 
 graph_1, map_1 = st.columns(2)
 graph_2, map_2 = st.columns(2)
 
+if not st.session_state.keys() >= {"fire_object_air", "fire_object_temp"}:
+    # with graph_1:
+    #     line1 = px.line(AirQuality_monthspan, x='date', y=AirQuality_monthspan.columns[1:5])
+    #     st.write(line1)
 
-with graph_1:
-	line1 = px.line(AirQuality_monthspan, x='date', y=AirQuality_monthspan.columns[1:5])
-	st.write(line1)
-    
+    # with graph_2:
+    #     line2 = px.line(AirQuality_before_and_after, x='date', y=AirQuality_before_and_after.columns[1:5])
+    #     st.write(line2)
 
-with graph_2:
-	line2 = px.line(AirQuality_before_and_after, x='date', y=AirQuality_before_and_after.columns[1:5])
-	st.write(line2)
+    with graph_1:
+        line1 = px.line().add_annotation(
+                                         text="No data available",
+                                         showarrow=False,
+                                         font=dict(
+                                             size=20,
+                                         ),
+                                         bordercolor="#c7c7c7",
+                                        borderwidth=1,
+                                        borderpad=4,
+                                        bgcolor="#eeeeee",
+                                        align="center")
+        st.write(line1)
 
+    with graph_2:
+        line2 = px.line().add_annotation(
+                                         text="No data available",
+                                         showarrow=False,
+                                         font=dict(
+                                             size=20,
+                                         ),
+                                         bordercolor="#c7c7c7",
+                                        borderwidth=1,
+                                        borderpad=4,
+                                        bgcolor="#eeeeee",
+                                        align="center")
+        st.write(line2)
 
-prev, _ ,next = st.columns([1, 10, 1])
-
-
-
-
-with map_1:
-    m = geemap.Map(location=[56.72769575738292, -111.3802457353699], zoom_start=10)
-    m.to_streamlit(
+    with map_1:
+        Map1 = geemap.Map(plugin_Draw=False)
+        Map1.to_streamlit(
             width=1000, height=500)
 
-with map_2:
-    m = geemap.Map(location=[56.72769575738292, -111.3802457353699], zoom_start=10)
-    m.to_streamlit(
+    with map_2:
+        Map2 = geemap.Map(plugin_Draw=False)
+        Map2.to_streamlit(
             width=1000, height=500)
+else:
+    st.session_state["fire_object"].Map.to_streamlit(
+        width=1500,
+        height=700)
 
+
+prev, _, next = st.columns([1, 10, 1])
 
 
 button_1, button_2, button_3 = st.columns(3)
@@ -121,14 +165,16 @@ button_1, button_2, button_3 = st.columns(3)
 
 with button_1:
     if st.button("Back to Boreal_Exploration"):
-         st.markdown('<meta http-equiv="refresh" content="0;url=/Boreal_Exploration">', unsafe_allow_html=True)
+        st.markdown(
+            '<meta http-equiv="refresh" content="0;url=/Boreal_Exploration">', unsafe_allow_html=True)
 
 with button_2:
     if st.button("View data and extra information on fire"):
-        st.markdown('<meta http-equiv="refresh" content="0;url=/Boreal_Exploration_Continued">', unsafe_allow_html=True)
-
+        st.markdown(
+            '<meta http-equiv="refresh" content="0;url=/Boreal_Exploration_Continued">', unsafe_allow_html=True)
 
 
 with button_3:
     if st.button("How to use "):
-        st.markdown('<meta http-equiv="refresh" content="0;url=/How_to_Use">', unsafe_allow_html=True)
+        st.markdown(
+            '<meta http-equiv="refresh" content="0;url=/How_to_Use">', unsafe_allow_html=True)
