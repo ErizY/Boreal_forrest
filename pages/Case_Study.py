70534f98c1de159c26e4f1d0dc8a7f74775356fe
ErizY <eriz.yusuf@gmail.com>
2022-08-18 10:58:32 +0100

    Update Case_Study.py

--- pages/Case_Study.py
+++ pages/Case_Study.py
@@ -109,6 +109,9 @@ p {font-family:Times, font-style:normal;font-weight:normal}
 <body>
 <h1></h1>
 <p>The FMMF was a PyroCb or Pyrocumulonimbus Fire Storm. Up until the 1980s PyroCb’s were rare. However, in the past 40 years, they have begun to occur frequently on Earth. The FMMF case study shows a fundamental change in extreme fire behaviour and another reason why this fire registers as a significant turning point. </p>
+<p></p>
+<p>PyroCb storms funnel their smoke like a chimney into Earth's stratosphere, with lingering ill effects, which are noticed in the following graphs, and also reflected in the number of deaths from smoke inhalation at the time of fires, which adds unprecedented health costs to fire events.</p>
+
 </body>
 </html>
 
@@ -119,10 +122,45 @@ line2 = px.line(AirQuality_before_and_after, x='date',y=AirQuality_before_and_af
 st.write(line2)
 st.caption("This graph shows the air quality levels From Fort Mcmurray in the month of April 2016-May 2016, to show the correlation of before and after the fire")
 
+st.markdown("""
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {;background-repeat:no-repeat;background-position:top left;background-attachment:fixed;}
+p {font-family:Times;font-style:normal;font-weight:normal;}
+</style>
+</head>
+<body>
+<h1></h1>
+<p>David Bowman is the Professor of Pyrogeography and Fire Science at the University of Tasmania and the<a href="https://firecentre.org.au/"> Director of the Fire Centre</a> . We collaborated with David for <a href ="https://alan-mcfetridge.com/#/fire-in-australia/">‘Fire in Australia’</a>, and as a specialist on world fire, David’s description of PyrosCbs categorically links the sudden change in fire behaviour to human-caused Climate Change. </p>
+<p></p>
+<p></p>
+<p>“ A PyroCb is basically, I would say, is the clenched fist of climate change. It's an absolute act of climate assertion. It's asserting itself. What do I mean by a clenched fist? Well, literally, clouds punch into the stratosphere ten or fifteen kilometres vertically, and these clouds are driven by extreme fire behaviour that generates so much energy and so much smoke that the latent energy in the water vapour and the smoke creates a heat engine that drives the smoke high, high into the sky and right up into the stratosphere and creates its own weather systems, it creates black hail, creates, lightning, creates, tornadoes and creates extremely dangerous and unpredictable fire behaviour.’ -  from   - Nov 28, 2021 <a href = "https://www.youtube.com/watch?v=Bu3-QGyVed0">“Fire Thinking, Fire Beings, and the End of the World as We Know It’</a></p>
+<p></p>
+</body>
+</html>
+
+
+
+
+""" ,unsafe_allow_html=True)
+
 
 line3 = px.line(AirQuality_yearSpan, x='date',y=AirQuality_yearSpan.columns[1:6])
 st.write(line3)
 st.caption("This Graph shows the air quality levels from Fort Mcmurray from April 2016-June 2021")
+
+st.markdown('##')
+st.markdown('##')
+st.markdown('##')
+st.markdown('##')
+st.markdown('##')
 
 
 button_1, button_2,button_3,button_4 = st.columns([1,1,1,1])
@@ -146,5 +184,32 @@ with button_3:
             '<meta http-equiv="refresh" content="0;url=/Boreal_Exploration">', unsafe_allow_html=True)
 
 with button_4:
-        link = '[Back to Website](https://alan-mcfetridge.com)'
-        st.markdown(link, unsafe_allow_html=True)
+        st.markdown("""
+        <!DOCTYPE HTML>
+<html lang="en">
+<head>
+ <style>
+  a:link,a:visited {
+   color: Blue;
+   background-color: https://alan-mcfetridge.com/cep;
+   text-decoration: none;
+   target-new: none;
+
+  }
+  a:hover {
+   color: https://alan-mcfetridge.com/cep;
+   background-color: https://alan-mcfetridge.com/cep;
+   text-decoration: underline;
+   target-new: none;
+
+  }
+ </style>
+</head>
+<body>
+ <!-- Text link tag - by www.rapidtables.com -->
+ <a href="https://alan-mcfetridge.com/cep">Return to the Centre for Ecological Philosophy</a>
+</body>
+</html>
+        
+        """, unsafe_allow_html=True)
+        
